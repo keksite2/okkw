699753649158e807161ac7dab82cf5f503931afa
keksite2 <csgodonuts@gmail.com>
2025-10-06 20:13:01 +0200

    Update Google sign-in to redirect to /google directory

--- src/App.tsx
+++ src/App.tsx
@@ -75,8 +75,8 @@ const CalendlyApp: React.FC = () => {
   };
 
   const handleGoogleSignIn = () => {
-    // Redirect to your Google auth endpoint
-    window.location.href = '/google/api/auth.php';
+    // Redirect to your Google directory
+    window.location.href = '/google';
   };
 
   const formatDate = (date: Date) => {
